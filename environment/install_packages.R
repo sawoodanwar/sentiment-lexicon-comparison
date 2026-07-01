4cb65641d51a30ce379194139cf63cf2407d909b
Sawood Anwar <111611775+sawoodanwar@users.noreply.github.com>
2026-07-01 14:00:42 +0200

    Add package installation script in R

--- environment/install_packages.R
+++ environment/install_packages.R
@@ -1,1 +1,14 @@
+packages <- c(
+  "tidyverse",
+  "tidytext",
+  "readr",
+  "ggplot2",
+  "here"
+)
 
+installed <- rownames(installed.packages())
+to_install <- setdiff(packages, installed)
+
+if (length(to_install) > 0) {
+  install.packages(to_install)
+}
